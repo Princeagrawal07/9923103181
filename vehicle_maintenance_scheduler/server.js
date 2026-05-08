85fe2bde8a8397ba0f686e77f146495bdc54341e
PrinceAgrawal <agrawalprince232@gmail.com>
2026-05-08 15:28:36 +0530

    Add notification system design stages

--- vehicle_maintenance_scheduler/server.js
+++ vehicle_maintenance_scheduler/server.js
@@ -3,10 +3,58 @@ const fetchData = require("./scheduler");
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const data = await fetchData();
+function knapsack(tasks, maxHours) {
+  const n = tasks.length;
 
-  res.json(data);
+  const dp = Array.from({ length: n + 1 }, () =>
+    Array(maxHours + 1).fill(0)
+  );
+
+  for (let i = 1; i <= n; i++) {
+    const duration = tasks[i - 1].Duration;
+    const impact = tasks[i - 1].Impact;
+
+    for (let w = 0; w <= maxHours; w++) {
+      if (duration <= w) {
+        dp[i][w] = Math.max(
+          impact + dp[i - 1][w - duration],
+          dp[i - 1][w]
+        );
+      } else {
+        dp[i][w] = dp[i - 1][w];
+      }
+    }
+  }
+
+  return dp[n][maxHours];
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const data = await fetchData();
+
+    const depots = data.depots.depots;
+    const vehicles = data.vehicles.vehicles;
+
+    const results = depots.map((depot) => {
+      const bestImpact = knapsack(
+        vehicles,
+        depot.MechanicHours
+      );
+
+      return {
+        depotId: depot.id,
+        mechanicHours: depot.MechanicHours,
+        maximumImpact: bestImpact,
+      };
+    });
+
+    res.json(results);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
 });
 
 app.listen(3000, () => {
